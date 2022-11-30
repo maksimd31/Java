06c94c597e41e3f68979b2172b4741f1bb05f8f3
Froge <maksimdis31@gmail.com>
2022-11-30 22:19:15 +0300

    2

---
+++ Homework/HomeWork_3/HomeWork_1.java
@@ -0,0 +1,29 @@
+
+
+public class HomeWork_1 {
+    /*
+    1.Реализовать алгоритм сортировки слиянием.
+     */
+//    public static void main(String[] args) {
+//        int[] Array = {4,3,4,31,11,34,567,};
+//        mergeSort(Array); //Создаем метод слияния
+////        System.out.println("");
+//    }
+//
+//    private static void mergeSort(int[] array) {
+//        int mass = array.length;
+//        int mid = mass/2; //Делим массив на 2
+//        int[] lev = new int[mid];
+//        int[] r = new int[lev - mid];
+//
+//        for (int i =0; i<mid;i++)
+//            lev[i] = array[i];
+//        for (int i =0; i<lev-mid;i++)
+//            r[i] = array[i+mid];
+//
+//
+//
+//        System.out.println(mid);
+//    }
+//
+}
